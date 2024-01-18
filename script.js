700b22ac39c37962d9a75ddf3112cf9d2d39c3fe
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2024-01-18 23:43:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let arr   = ["#"];
+	for(let i=1; i<shortHex.length;i++){
+		arr.push(shortHex.charAt(i));
+		arr.push(shortHex.charAt(i));
+	}
+
+	return arr.join("");
 };
 
 // Do not change the code below.
